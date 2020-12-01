fe34e2b7b3f232861e9bdd741db41b2c6f70735e
Juan Pablo Garzon <jgarzonebrath08@gmail.com>
2020-12-01 08:23:18 -0500

    Application compiled and ready to deploy

---
+++ src/components/CompanyContact/CompanyContactEdit.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Textfield from "@smui/textfield";
+    import HelperText from "@smui/textfield/helper-text";
+    import Select, {Option} from "@smui/select";
+    import SelectHelperText from "@smui/select/helper-text";
+    import Whatsapp from 'svelte-material-icons/Whatsapp.svelte';
+</script>
+
+<style>
+
+</style>
+
+<div class="CompanyContactEdit">
+
+</div>
